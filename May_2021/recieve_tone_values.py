40a32260479e4fe5dfd2165699419fa730a69dcb
emmalala123 <emmalala@brandeis.edu>
2021-05-25 15:19:02 -0400

    blocks_sounds is integrated with receive.py, still in testing

--- May_2021/recieve_tone_values.py
+++ May_2021/recieve_tone_values.py
@@ -6,6 +6,9 @@ This program recieves values from send_tone_value.py on the cue RPi
 
 import socket
 
+def get_value(int):
+        return int
+
 UDP_IP = "129.64.50.48"
 UDP_PORT = 5005
 
@@ -18,3 +21,6 @@ while True:
         data, addr = sock.recvfrom(1024) #buffer size is 1024 bytes
         # send this to function that initiates tone/replace keyboard values
         print"recieved message:", int(data)
+        get_value(int(data))
+        
+        
--- May_2021/send.py
+++ May_2021/send.py
@@ -1,15 +1,22 @@
+from send_tone_value import UDP_IP
 import socket
+import random
 
-UDP_IP = "129.64.50.48"
-UDP_PORT = 5005
+def send_tone(value):
+    #UDP_IP = "129.64.50.48"
+    #when on phone
+    UDP_IP = "172.20.10.8"
+    UDP_PORT = 5005
 
-MESSAGE = "2"
+    MESSAGE = value
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+    print "UDP target IP:", UDP_IP
+    print "UDP target port:", UDP_PORT
+    print "message:", MESSAGE
 
-sock = socket.socket(socket.AF_INET, #internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-
+    sock = socket.socket(socket.AF_INET, #internet
+                        socket.SOCK_DGRAM) # UDP
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+    
+value = random.randint(0,4)
+send_tone(str(value))
