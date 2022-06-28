e49df5e3508ba0c042740d27facbdfb29b4ef825
danielsvedberg <dsvedberg@brandeis.edu>
2022-06-28 17:08:17 -0400

    bugfix

--- cuedtaste_2_0.py
+++ cuedtaste_2_0.py
@@ -319,7 +319,7 @@ def cuedtaste():
             print("new trial")
 
         while state == 1 and time.time() <= endtime:  # state 1: new trial started/arming Trigger
-
+            print("state 1")
             if trig.is_crossed():  # once the trigger-nosepoke is crossed, move to state 2
                 trig_run.value = 2  # trigger light goes from blinking to just on
                 lines[line].play_cue()  # taste-associated cue cue is played
